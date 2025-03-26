b3d01b2adfb77cde5976cd015257cc1257a1e23f
madhuk54 <apoorvav2006@gmail.com>
2025-03-26 11:55:52 +0530

    Adding a file

---
+++ sktest2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class content {
+    public static void main (String[] args){
+        String science = ["data Analysis","Programming"];
+        String math = ["Matrix","Geometry"];
+        String figma = ["User Interface"];
+        System.out.print("Prefer Your Content");
+    }
+}
